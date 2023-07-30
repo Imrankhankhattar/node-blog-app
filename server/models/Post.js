468811c90001bb9a81d75328e896c8744b7082ff
imran_khan_941 <imran.khan@tkxel.io>
2023-07-30 20:28:50 +0500

    post getting rendered on home page

---
+++ server/models/Post.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const PostSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required:true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+    modifiedAt:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Post', PostSchema)
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,30 +1,104 @@
 const express = require('express')
-const router  = express.Router()
+const router = express.Router()
+const Post = require('../models/Post')
+
+router.get('/', async (req, res) => {
+
+    try {
+        let postPerPage = 10
+        let page = req.query.page || 1
+        const pagePosts = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+        .skip(postPerPage * page - postPerPage)
+        .limit(postPerPage)
+        .exec()
+        const count = await Post.count()
+        const nextPage = parseInt(page) + 1
+        const hasNextPage = nextPage <= (count / postPerPage)
+        res.render('index', {
+            posts: pagePosts,
+            nextPage: hasNextPage ? nextPage : null,
+            current: page
+        })
+    } catch (error) {
+        console.log('error', error)
+    }
+})
+router.get('/about', (req, res) => {
+    const data = {
+        name: 'John Doe',
+        age: 30,
+        address: '123 Main St'
+    }
+    res.render('about', { data })
+})
+
+function insertPostData() {
+    Post.insertMany([
+        {
+            title: "Building APIs with Node.js 3",
+            description: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js"
+        },
+        {
+            title: "Deployment of Node.js applications",
+            description: "Understand the different ways to deploy your Node.js applications, including on-premises, cloud, and container environments..."
+        },
+        {
+            title: "Authentication and Authorization in Node.js",
+            description: "Learn how to add authentication and authorization to your Node.js web applications using Passport.js or other authentication libraries."
+        },
+        {
+            title: "Understand how to work with MongoDB and Mongoose",
+            description: "Understand how to work with MongoDB and Mongoose, an Object Data Modeling (ODM) library, in Node.js applications."
+        },
+        {
+            title: "build real-time, event-driven applications in Node.js",
+            description: "Socket.io: Learn how to use Socket.io to build real-time, event-driven applications in Node.js."
+        },
+        {
+            title: "Discover how to use Express.js",
+            description: "Discover how to use Express.js, a popular Node.js web framework, to build web applications."
+        },
+        {
+            title: "Asynchronous Programming with Node.js",
+            description: "Asynchronous Programming with Node.js: Explore the asynchronous nature of Node.js and how it allows for non-blocking I/O operations."
+        },
+        {
+            title: "Learn the basics of Node.js and its architecture",
+            description: "Learn the basics of Node.js and its architecture, how it works, and why it is popular among developers."
+        },
+        {
+            title: "NodeJs Limiting Network Traffic",
+            description: "Learn how to limit netowrk traffic."
+        },
+        {
+            title: "Learn Morgan - HTTP Request logger for NodeJs",
+            description: "Learn Morgan."
+        },
+    ])
+}
+
+// insertPostData();
 
 
-router.get('/',(req,res)=>{
+
+
+
+
+
+
+
+
+
+
+
+
+router.get('/contact', (req, res) => {
     const data = {
         name: 'John Doe',
         age: 30,
         address: '123 Main St'
     }
-    res.render('index',{data})
-})
-router.get('/about',(req,res)=>{
-    const data = {
-        name: 'John Doe',
-        age: 30,
-        address: '123 Main St'
-    }
-    res.render('about',{data})
-})
-router.get('/contact',(req,res)=>{
-    const data = {
-        name: 'John Doe',
-        age: 30,
-        address: '123 Main St'
-    }
-    res.render('contact',{data})
+    res.render('contact', { data })
 })
 
 
